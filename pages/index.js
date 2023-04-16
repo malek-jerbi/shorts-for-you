9a173cd470b0c5874cfa2023012d25eb9b16a46b
Malek Jerbi <malek.jerbi@gmail.com>
2023-04-16 18:27:20 -0400

    remove icon

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,6 @@ export default function Home() {
     <div>
       <Head>
         <title>Podcast Transcript Search</title>
-        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
